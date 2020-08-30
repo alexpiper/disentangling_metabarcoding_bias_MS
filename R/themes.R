3f2386850b2be061f81c3f20c57fd12aac018944
alexpiper <alexpiperdesigns@gmail.com>
2020-08-30 10:11:34 +1000

    Added bioinformatics rmd

--- R/themes.R
+++ R/themes.R
@@ -1,5 +1,4 @@
 # Define themes
-library(ggthemes)
 base_theme <- theme_minimal()+
   theme(
     strip.background = element_blank(),
@@ -10,7 +9,7 @@ base_theme <- theme_minimal()+
     plot.background = element_blank(),
     text = element_text(size=9, family = ""),
     axis.text = element_text(size=8, family = ""),
-    legend.position = "none",
+    legend.position = "bottom",
     panel.border = element_rect(colour = "black", fill=NA, size=0.5),
     panel.grid = element_line(size = rel(0.5))
   )
